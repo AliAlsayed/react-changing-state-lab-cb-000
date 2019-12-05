c4dd6ad7d60eb841db28bd9651e772d79150618d
Ali Alsayed <a_alsayed87@hotmail.com>
2019-12-05 09:01:07 +0000

    Automatically backed up by Learn

--- components/Game.js
+++ components/Game.js
@@ -6,27 +6,46 @@ import solutions from './solutions';
 export default class Game extends React.Component {
   constructor (props) {
     super(props);
-    this.state = {};
+    this.state = {
+      turn: 'X',
+      board: [null, null, null, null, null, null, null, null, null]
+    };
 
     this.handleReset = this.handleReset.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleReset (ev) {
+    ev.preventDefault()
+    this.setState({
+      board: [null, null, null, null, null, null, null, null, null]
+    })
   }
 
   handleClick (i, ev) {
+    ev.preventDefault()
   }
 
   getWinner () {
+    const turn = this.state.turn
+    for(var i = 0; i < solutions.length; i++){
+      const comb = [this.state.board[solutions[i][0]], this.state.board[solutions[i][1]], this.state.board[solutions[i][2]]]
+      if (comb === [turn, turn, turn]){
+        return `${turn}`
+      }
+    }
   }
 
   isComplete () {
+    return getWinner()
   }
 
   render () {
     return (
       <div>
+        <Board turn={this.state.turn} board={this.state.board} onClick={this.handleClick}/>
+        <Status winner={this.getWinner} isComplete={this.isComplete}/>
+        <button className="game__reset" onClick={this.handleReset}></button>
       </div>
     );
   }
--- components/Status.js
+++ components/Status.js
@@ -2,9 +2,10 @@ import React from 'react';
 
 export default class Status extends React.Component {
   render () {
-    const { winner } = this.props;
+    const { winner, isComplete } = this.props;
     return (
       <div>
+        { winner ? `${winner} wins` : 'Tie'}
       </div>
     );
   }
