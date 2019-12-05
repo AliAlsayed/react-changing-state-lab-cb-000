62bc5853178a8803f080186e354ae465e6dc3bed
Ali Alsayed <a_alsayed87@hotmail.com>
2019-12-05 10:17:37 +0000

    Automatically backed up by Learn

--- components/Field.js
+++ components/Field.js
@@ -4,7 +4,7 @@ export default class Field extends React.Component {
   render () {
     const { player, onClick, index } = this.props;
     return (
-      <button onClick={onClick} disabled={!!player}>
+      <button className='field' onClick={onClick} disabled={!!player}>
         {player}
       </button>
     );
