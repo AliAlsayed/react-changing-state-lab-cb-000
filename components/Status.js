728389aafe5bfe0000363a8eb7dbdc37f9994d04
Ali Alsayed <a_alsayed87@hotmail.com>
2019-12-05 09:30:47 +0000

    Automatically backed up by Learn

--- components/Status.js
+++ components/Status.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 export default class Status extends React.Component {
   render () {
-    const { winner, isComplete } = this.props;
+    const { winner } = this.props;
     return (
       <div>
-        { isComplete }
+        { winner ? `${winner} wins` : 'Tie'}
       </div>
     );
   }
